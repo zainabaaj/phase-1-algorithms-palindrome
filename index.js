8b8dde123994d1e006a6efedc4483195839fee04
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-27 17:10:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedStr = word.split('').reverse().join('');
+  return reversedStr === word
 }
 
 /* 
   Add your pseudocode here
+  const reversed
+  reversed = word.tochar()
+  reversed.reverse()
+  if reversed === word
+  return true
+  return false 
 */
 
 /*
   Add written explanation of your solution here
+  we need to check if the word is a palindrome 
+  which means that the word spelling backwards will be the same 
+  so first we need to reverse the word 
+  change the string into characters
+  reverse them 
+  then put them back as a string 
+  and check if you have the same word
+
 */
 
 // You can run `node index.js` to view these console logs
